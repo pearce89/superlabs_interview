6c341112334badc54748bc7a5a5fe75b2d922190
Sergei Anisimov <anisimov@barrelsdev.com>
2017-05-16 02:38:34 +0300

    basic comments implementation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    create_comment = Movies::CreateComment.run(
+                      params.require(:comment)
+                            .merge(user: current_user.uuid)
+                            .permit!)
 
+    @movie  = create_comment.result[:movie]
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 class Comment
   include Neo4j::ActiveNode
   property :body, type: String
+  property :updated_at
+  property :created_at
+
+  validates :body, presence: true
+
   has_one :out, :movie, type: :movie
   has_one :out, :user,  type: :user
 
